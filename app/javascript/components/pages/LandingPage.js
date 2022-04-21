142bda6545029fc8d8d063d779780d819d2baeb4
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-04-21 16:29:38 -0700

    Landing Page countdown timer until presentation to motivate our devs here at Code Monkeys

--- app/javascript/components/pages/LandingPage.js
+++ app/javascript/components/pages/LandingPage.js
@@ -4,22 +4,73 @@ import { Button } from 'reactstrap'
 
 
 class LandingPage extends Component {
+  constructor(props) {
+    super(props)
+    this.startCountDown()
+    this.state = {
+      time: {
+        days: null,
+        hours: null,
+        minutes: null,
+        seconds: null
+      }
+    }
+  }
+
+  startCountDown = () => {
+    const countDownDate = new Date("May 2, 2022 12:00:00").getTime()
+    console.log(countDownDate)
+    setInterval(() => {
+
+      // Get today's date and time
+      const now = new Date().getTime()
+
+      // Find the distance between now and the count down date
+      const distance = countDownDate - now
+
+      // Time calculations for days, hours, minutes and seconds
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+      const { time } = this.state
+      time.days = days
+      time.hours = hours
+      time.minutes = minutes
+      time.seconds = seconds
+      this.setState({ time })
+    }, 1000)
+  }
 
   render() {
+    const { time } = this.state
     return (
       <>
-
-        <div class="section" data-name="section">
-          <section class="section-2">
-            <div class="div" data-name="div-1">
-              <section class="section-3"></section>
+        <div className="section" data-name="section">
+          <section className="section-2">
+            <div className="div" data-name="div-1">
+              <section className="section-3"></section>
             </div>
-            <div class="div-2">
-              <div class="div-3"><p>Something Great, Coming Soon</p></div>
-              <div class="div-4"><p>05 - 02 - 2022</p></div>
-              <div class="div-5"></div>
-              <div class="div-6">
-                <div class="div-7">
+            <div className="div-2">
+              <div className="div-3"><p>Something Great, Coming Soon</p></div>
+              <div className="div-4"><p>05 - 02 - 2022</p></div>
+              <div className='div-5' style={{ color: 'white', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+                <span style={{ margin: '10px' }}>
+                  Days: {time.days || '00'}
+                </span>
+                <span style={{ margin: '10px' }}>
+                  Hours: {time.hours || '00'}
+                </span>
+                <span style={{ margin: '10px' }}>
+                  Minutes: {time.minutes || '00'}
+                </span>
+                <span style={{ margin: '10px' }}>
+                  Seconds: {time.seconds || '00'}
+                </span>
+              </div>
+              <div className="div-6">
+                <div className="div-7">
                   <NavLink to='/signup'>
                     <Button
                       id="signUpButton"
