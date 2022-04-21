6dd41c25e564ac9e75f2ccaaf46ac389b3fa7fd8
josesison1 <learnacademy@LEARNs-Air.lan>
2022-04-21 10:47:42 -0700

    completed footer

---
+++ app/javascript/components/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+
+  render() {
+    return (
+      <>
+        <div className="footer">
+          <p>© 2022 | onClick() Application | Capstone Project created by Dane, Jose, Kat, Nico. </p>
+        </div>
+      </>
+    )
+  }
+}
+
+export default Footer
+
