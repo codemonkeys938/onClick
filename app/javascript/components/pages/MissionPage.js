88e8569b17b47f80148c03c61b2cf79b20d30483
josesison1 <learnacademy@LEARNs-Air.lan>
2022-04-22 10:23:02 -0700

    completed mission page

---
+++ app/javascript/components/pages/MissionPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class MissionPage extends Component {
+
+  render() {
+    return (
+      <>
+        <div className="missionpage">
+          <h1>Our Mission</h1>
+          <p>Want to connect with other people that are more like you? onClick is an application where you can create or join groups based on your interests. onClick allows our users to find connections and friendships through group messaging boards. Find a new friend, your next business partner, a mentor or someone just to chat with for a little. onClick makes it possible and easy.</p>
+        </div>
+      </>
+    )
+  }
+}
+
+export default MissionPage
