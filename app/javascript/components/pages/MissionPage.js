56dad3c8c1c9f4a86dcbc781d357ecdf04d326b3
josesison1 <learnacademy@LEARNs-Air.lan>
2022-04-27 16:11:58 -0700

    completed mission branch

--- app/javascript/components/pages/MissionPage.js
+++ app/javascript/components/pages/MissionPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import logo from '../assets/OnClickLogoDraft.png'
 
 
 class MissionPage extends Component {
@@ -8,7 +9,11 @@ class MissionPage extends Component {
       <>
         <div className="missionpage">
           <h1>Our Mission</h1>
-          <p>Want to connect with other people that are more like you? onClick is an application where you can create or join groups based on your interests. onClick allows our users to find connections and friendships through group messaging boards. Find a new friend, your next business partner, a mentor or someone just to chat with for a little. onClick makes it possible and easy.</p>
+          <div className="missiontext">
+            <p>Want to connect with other people that are more like you? onClick is an application where you can create or join groups based on your interests. onClick allows our users to find connections and friendships through group messaging boards. Find a new friend, your next business partner, a mentor or someone just to chat with for a little. onClick makes it possible and easy.</p></div>
+          <div className="missionlogo">
+            <img src={logo} width="280px" />
+          </div>
         </div>
       </>
     )
