7c1088786a9010761d8ee298aff7e88ac6133f1e
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-05-03 10:04:12 -0700

    the final touches, removed countdown clock from landing page

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -1,110 +1,100 @@
-import React, { Component } from 'react'
-import { Card, CardGroup, CardImg, CardSubtitle, CardTitle, CardBody, CardText } from 'reactstrap'
-import joseicon from '../assets/hi.png'
-import katicon from '../assets/my cat says hi.png'
-import daneicon from '../assets/File.png'
-
+import React, { Component } from "react"
+import {
+  Card,
+  CardGroup,
+  CardImg,
+  CardSubtitle,
+  CardTitle,
+  CardBody,
+  CardText
+} from "reactstrap"
+import joseicon from "../assets/hi.png"
+import katicon from "../assets/my cat says hi.png"
+import daneicon from "../assets/File.png"
 
 class AboutUs extends Component {
-
   render() {
     return (
-      <><div className="aboutpage">
-        <h1>About Us</h1>
-        <div className="aboutuscards">
-          <CardGroup>
-            <Card>
-              <CardImg
-                alt="Card image cap"
-                src="https://avatars.githubusercontent.com/u/99694167?v=4"
-                top
-                width="100%"
-              />
-              <CardBody>
-                <CardTitle tag="h5">
-                  <strong className="aboutusname">Nico</strong>
-                </CardTitle>
-                <CardSubtitle
-                  className="mb-2 text-muted"
-                  tag="h6"
-                >
-                  Product Manager
-                </CardSubtitle>
-                <CardText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </CardText>
-              </CardBody>
-            </Card>
-            <Card>
-              <CardImg
-                alt="Card image cap"
-                src={joseicon}
-                top
-                width="100%"
-              />
-              <CardBody>
-                <CardTitle tag="h5">
-                  <strong className="aboutusname">Jose</strong>
-                </CardTitle>
-                <CardSubtitle
-                  className="mb-2 text-muted"
-                  tag="h6"
-                >
-                  Project Manager
-                </CardSubtitle>
-                <CardText>
-                  That rewarding feeling you get after creating something from a blank canvas is the best and the main reason why I got into software development. Outside of programming, I like to spend time with my family and eat tacos all day. “If it aint broke, don't fix it” - Bert Lance
-                </CardText>
-              </CardBody>
-            </Card>
-            <Card>
-              <CardImg
-                alt="Card image cap"
-                src={daneicon}
-                top
-                width="100%"
-              />
-              <CardBody>
-                <CardTitle tag="h5">
-                  <strong className="aboutusname">Dane</strong>
-                </CardTitle>
-                <CardSubtitle
-                  className="mb-2 text-muted"
-                  tag="h6"
-                >
-                  Tech Lead
-                </CardSubtitle>
-                <CardText>
-                  Puzzle enthusiast that enjoys taking on new and unique problems.  When I'm not ruining my posture sitting at a computer all day, you find me walking along the beach or playing catch with my friends.  "Never memorize something that you can look up." ― Albert Einstein
-                </CardText>
-              </CardBody>
-            </Card>
-            <Card>
-              <CardImg
-                alt="Card image cap"
-                src={katicon}
-                top
-                width="100%"
-              />
-              <CardBody>
-                <CardTitle tag="h5">
-                  <strong className="aboutusname">Kat</strong>
-                </CardTitle>
-                <CardSubtitle
-                  className="mb-2 text-muted"
-                  tag="h6"
-                >
-                  Design Lead
-                </CardSubtitle>
-                <CardText>
-                  UX/UI/Random backgrounds, Keeper of The Wireframes, and Guardian of CSS. I draw pictures and occasionally do code. When I'm not doing mock-ups or UI, I'm petting cats and wondering if they hate me. “Learn the rules like a pro, so you can break them like an artist” ~Pablo Picasso"
-                </CardText>
-              </CardBody>
-            </Card>
-          </CardGroup>
+      <>
+        <div className="aboutpage">
+          <h1>About Us</h1>
+          <div className="aboutuscards">
+            <CardGroup>
+              <Card>
+                <CardImg
+                  alt="Card image cap"
+                  src="https://avatars.githubusercontent.com/u/99694167?v=4"
+                  top
+                  width="100%"
+                />
+                <CardBody>
+                  <CardTitle tag="h5">
+                    <strong className="aboutusname">Nico</strong>
+                  </CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Product Manager
+                  </CardSubtitle>
+                  <CardText>"WIP"</CardText>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardImg alt="Card image cap" src={joseicon} top width="100%" />
+                <CardBody>
+                  <CardTitle tag="h5">
+                    <strong className="aboutusname">Jose</strong>
+                  </CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Project Manager
+                  </CardSubtitle>
+                  <CardText>
+                    That rewarding feeling you get after creating something from
+                    a blank canvas is the best and the main reason why I got
+                    into software development. Outside of programming, I like to
+                    spend time with my family and eat tacos all day. “If it aint
+                    broke, don't fix it” - Bert Lance
+                  </CardText>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardImg alt="Card image cap" src={daneicon} top width="100%" />
+                <CardBody>
+                  <CardTitle tag="h5">
+                    <strong className="aboutusname">Dane</strong>
+                  </CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Tech Lead
+                  </CardSubtitle>
+                  <CardText>
+                    Puzzle enthusiast that enjoys taking on new and unique
+                    problems. When I'm not ruining my posture sitting at a
+                    computer all day, you find me walking along the beach or
+                    playing catch with my friends. "Never memorize something
+                    that you can look up." ― Albert Einstein
+                  </CardText>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardImg alt="Card image cap" src={katicon} top width="100%" />
+                <CardBody>
+                  <CardTitle tag="h5">
+                    <strong className="aboutusname">Kat</strong>
+                  </CardTitle>
+                  <CardSubtitle className="mb-2 text-muted" tag="h6">
+                    Design Lead
+                  </CardSubtitle>
+                  <CardText>
+                    UX/UI/Random backgrounds, Keeper of The Wireframes, and
+                    Guardian of CSS. I draw pictures and occasionally do code.
+                    When I'm not doing mock-ups or UI, I'm petting cats and
+                    wondering if they hate me. “Learn the rules like a pro, so
+                    you can break them like an artist” ~Pablo Picasso"
+                  </CardText>
+                </CardBody>
+              </Card>
+            </CardGroup>
+          </div>
+          <div className="background-image missionpage"></div>
         </div>
-        <div className="background-image missionpage"></div>
-      </div>
       </>
     )
   }
--- app/javascript/components/pages/LandingPage.js
+++ app/javascript/components/pages/LandingPage.js
@@ -6,50 +6,7 @@ import landing from "../assets/landing.png"
 import landingChopped from "../assets/Landing-chopped.png"
 
 class LandingPage extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      time: {
-        days: null,
-        hours: null,
-        minutes: null,
-        seconds: null,
-      },
-    }
-  }
-
-  componentDidMount() {
-    this.startCountDown()
-  }
-
-  startCountDown = () => {
-    const countDownDate = new Date("May 2, 2022 12:00:00").getTime()
-    setInterval(() => {
-      // Get today's date and time
-      const now = new Date().getTime()
-
-      // Find the distance between now and the count down date
-      const distance = countDownDate - now
-
-      // Time calculations for days, hours, minutes and seconds
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24))
-      const hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      )
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000)
-
-      const { time } = this.state
-      time.days = days.toString().length === 1 ? `0${days}` : days
-      time.hours = hours.toString().length === 1 ? `0${hours}` : hours
-      time.minutes = minutes.toString().length === 1 ? `0${minutes}` : minutes
-      time.seconds = seconds.toString().length === 1 ? `0${seconds}` : seconds
-      this.setState({ time: time })
-    }, 1000)
-  }
-
   render() {
-    const { time } = this.state
     return (
       <>
         <div className="btn-container landing-page-btns">
@@ -59,38 +16,6 @@ class LandingPage extends Component {
           <NavLink to="/signin" className="nl">
             <Button id="signUpButton">Sign In</Button>
           </NavLink>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <h3
-            style={{
-              textAlign: "right",
-              marginTop: "100px",
-              marginRight: "120px",
-            }}
-          >
-            Time Until Something Great
-          </h3>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "right",
-              marginRight: "120px",
-            }}
-          >
-            <span style={{ margin: "10px" }}>Days: {time.days || "00"}</span>
-            <span style={{ margin: "10px" }}>Hours: {time.hours || "00"}</span>
-            <span style={{ margin: "10px" }}>
-              Minutes: {time.minutes || "00"}
-            </span>
-            <span style={{ margin: "10px" }}>
-              Seconds: {time.seconds || "00"}
-            </span>
-          </div>
         </div>
         <img
           src={window.innerWidth >= 600 ? landing : landingChopped}
